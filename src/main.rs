37fd50b7e83a9db6b7512a7180361fb1dda41fba
peeeuzin <canaldopedrinho233@gmail.com>
2024-03-24 12:34:59 -0300

    feat: add `add` command to create other git hooks

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,48 @@ use std::os::unix::prelude::OpenOptionsExt;
 use std::path;
 use std::process::Command;
 
+const ALLOWED_HOOKS: [&str; 13] = [
+    "applypatch-msg",
+    "commit-msg",
+    "post-update",
+    "pre-applypatch",
+    "pre-commit",
+    "pre-merge-commit",
+    "pre-push",
+    "pre-rebase",
+    "pre-receive",
+    "prepare-commit-msg",
+    "push-to-checkout",
+    "sendemail-validate",
+    "update",
+];
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let command = &args[1];
+    if args.len() < 2 {
+        help();
 
-    run_command(command);
+        return;
+    }
+
+    let command = &args[1];
+    let args = args[2..].to_vec();
+
+    run_command(command, args);
 }
 
-fn run_command(command: &str) {
+fn run_command(command: &str, args: Vec<String>) {
     match command {
-        "init" => init(),
+        "init" => init(args.contains(&"--no-pre-commit".to_string())),
         "uninstall" => uninstall(),
+        "add" => add_hook(&args[0]),
 
         _ => println!("Command not found"),
     }
 }
 
-fn init() {
+fn init(no_pre_commit: bool) {
     let pre_commit_dir = path::Path::new(".hooky");
 
     if !path::Path::new(".git").exists() {
@@ -42,17 +66,19 @@ fn init() {
 
     fs::create_dir_all(pre_commit_dir).expect("Failed to create .hooky directory");
 
-    let mut file = fs::OpenOptions::new()
-        .create(true)
-        .write(true)
-        .mode(0o755)
-        .open(pre_commit_dir.join("pre-commit"))
-        .expect("Failed to create pre-commit file");
+    if !no_pre_commit {
+        let mut file = fs::OpenOptions::new()
+            .create(true)
+            .write(true)
+            .mode(0o755)
+            .open(pre_commit_dir.join("pre-commit"))
+            .expect("Failed to create pre-commit file");
 
-    file.write_all(b"#!/usr/bin/env sh\n# Run pre-commit hooks\n\nexit 0")
-        .expect("Failed to write to pre-commit file");
+        file.write_all(b"#!/usr/bin/env sh\n# Run pre-commit hooks\n\nexit 0")
+            .expect("Failed to write to pre-commit file");
 
-    println!("[info] Created pre-commit file");
+        println!("[info] Created pre-commit file");
+    }
 }
 
 fn uninstall() {
@@ -60,6 +86,12 @@ fn uninstall() {
 
     if !path::Path::new(".git").exists() {
         println!("[error] git cannot be found");
+
+        return;
+    }
+
+    if !pre_commit_dir.exists() {
+        println!("[info] hooky is not installed");
 
         return;
     }
@@ -74,4 +106,46 @@ fn uninstall() {
     fs::remove_dir_all(pre_commit_dir).expect("Failed to remove .hooky directory");
 
     println!("[info] Uninstalled hooky");
+}
+
+fn add_hook(hook: &str) {
+    if !ALLOWED_HOOKS.contains(&hook) {
+        println!("[error] Hook not allowed");
+
+        return;
+    }
+
+    let pre_commit_dir = path::Path::new(".hooky");
+
+    if !pre_commit_dir.exists() {
+        println!("[error] .hooky directory not found");
+        println!("[hint] try running `hooky init` first");
+
+        return;
+    }
+
+    if !path::Path::new(".git").exists() {
+        println!("[error] git cannot be found");
+
+        return;
+    }
+
+    let mut file = fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .mode(0o755)
+        .open(pre_commit_dir.join(hook))
+        .expect("Failed to create hook file");
+
+    let hook_content = format!("#!/usr/bin/env sh\n# Run {} hook\n\nexit 0", hook);
+    file.write_all(hook_content.as_bytes())
+        .expect("Failed to write to hook file");
+
+    println!("[info] Created hook file");
+}
+
+fn help() {
+    println!("hooky init - Initialize hooky. Optionally pass --no-pre-commit to skip creating a pre-commit hook");
+    println!("hooky uninstall - Uninstall hooky");
+    println!("hooky add <hook> - Add a hook");
 }
